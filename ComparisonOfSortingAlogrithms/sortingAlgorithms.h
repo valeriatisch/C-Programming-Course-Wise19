230e4a3de93b637d2cb9cca458ecc27acbb61ede
Valeria Tisch <57265123+valeriatisch@users.noreply.github.com>
2020-03-24 21:03:05 +0100

    Add files via upload

---
+++ ComparisonOfSortingAlogrithms/sortingAlgorithms.h
@@ -0,0 +1,16 @@
+//
+// Created by valeria on 13.01.20.
+//
+
+#ifndef COMPARISONOFSORTINGALGORITHMS_SORTINGALGORITHMS_H
+#define COMPARISONOFSORTINGALGORITHMS_SORTINGALGORITHMS_H
+
+#include "../include/auxiliaryFuncs.h"
+
+void bubbleSort(int array[], int n);
+void insertionSort(int array[], int n);
+void selectionSort(int array[], int n);
+void mergeSort(int array[], int l, int r);
+void quickSort(int array[], int low, int high);
+
+#endif //COMPARISONOFSORTINGALGORITHMS_SORTINGALGORITHMS_H
