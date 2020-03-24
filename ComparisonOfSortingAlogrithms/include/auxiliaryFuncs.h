74e7bc0a6f84c53dd73345adb2eaf633f3663dd1
Valeria Tisch <57265123+valeriatisch@users.noreply.github.com>
2020-03-24 21:04:01 +0100

    Add files via upload

---
+++ ComparisonOfSortingAlogrithms/include/auxiliaryFuncs.h
@@ -0,0 +1,15 @@
+//
+// Created by valeria on 13.01.20.
+//
+
+#ifndef COMPARISONOFSORTINGALGORITHMS_AUXILIARYFUNCS_H
+#define COMPARISONOFSORTINGALGORITHMS_AUXILIARYFUNCS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *xp, int *yp);
+void merge(int array[], int l, int m, int r);
+int partition (int array[], int low, int high);
+
+#endif //COMPARISONOFSORTINGALGORITHMS_AUXILIARYFUNCS_H
